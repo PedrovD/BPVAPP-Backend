c1a1a31a95a3225f4e7835ea8106f92e18675cae
Boris <b.mulder@appec.nl>
2019-09-11 20:24:18 +0200

    Create ModelMappings.cs
    added modelmapping file for database models

---
+++ BPVAPP-Backend/Database/ModelMappings.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BPVAPP_Backend.Database
+{
+    // Place all the mappings of models in this.
+}
