13837c0d06d4fa60cee257ec7b3403650e0e11fe
Boris <b.mulder@appec.nl>
2019-09-11 20:24:44 +0200

    Create DbConnection.cs
    added database connection class

---
+++ BPVAPP-Backend/Database/DbConnection.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using FluentNHibernate.Cfg;
+using NHibernate;
+using FluentNHibernate.Cfg.Db;
+using BPVAPP_Backend.Database.Models;
+using NHibernate.Tool.hbm2ddl;
+using System.Linq;
+
+namespace BPVAPP_Backend.Database
+{
+    public class DbConnection
+    {
+        private readonly ISessionFactory sessionFactory;
+
+        private static ISessionFactory CreateSessionFactory()
+        {
+            return Fluently.Configure()
+                .Database(MySQLConfiguration.Standard.ConnectionString("")). // Pedro will place the connection string
+                Mappings(m => m.FluentMappings.AddFromAssemblyOf<EmptyModel>()).
+                ExposeConfiguration(cfg => new SchemaUpdate(cfg).Execute(true, true)).
+                BuildSessionFactory();
+        }
+
+        public DbConnection()
+        {
+            sessionFactory = CreateSessionFactory();
+        }
+
+        /// <summary>
+        /// Simple methode to get all records from a table, you can use this method on all tables because its
+        /// a genereric function
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public List<T> GetAllModels<T>()
+        {
+            /// example use age
+            /// var db = new DbConnection()
+            /// List<CarModel> cars = db.GetAllModels<Cars>();
+            /// List<PlaneModel> cars = db.GetAllModels<Plane>();
+            /// 
+            using (var session = sessionFactory.OpenSession())
+            {
+                using (var trans = session.BeginTransaction())
+                {
+                    return session.Query<T>().ToList();
+                }
+            }
+        }
+
+        
+    }
+}
