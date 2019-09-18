b9bf3437ae5b3732ea1dc1ab5b7b95db3c39101c
joostjasper <joostjasper@live.nl>
2019-09-18 15:51:01 +0200

    Variabelen aangemaakt
    Meerdere variablen aangemaakt

---
+++ BPVAPP-Backend/Database/Models/LeerlingModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BPVAPP_Backend.Database.Models
+{
+        // This model is ussed for now as a refference for fluentnhibernate,
+        public class LeerlingModel
+        {
+            public virtual int Id { get; protected set; }
+            public virtual string FirstName { get; set; }
+            public virtual string LastName { get; set; }
+            public virtual int CatagoryId { get; set; }
+            public virtual int BegeleiderId { get; set; }
+            public virtual string Postcode { get; set; }
+            public virtual string Mail { get; set; }
+            public virtual string Telephone { get; set; }
+            public virtual string Password { get; set; }
+        }
+        // Can be removed later when we are using real models
+    
+}
