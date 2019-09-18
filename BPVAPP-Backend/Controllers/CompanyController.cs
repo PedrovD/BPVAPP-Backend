c3099071888776178de4ca4ef488f4b32357ccca
PedrovD <pedro_vandouveren@hotmail.com>
2019-09-18 15:35:21 +0200

    Added Model and Mapping
    added Company Model and Mapped it for Database. Also added the first function for the controller (AddCompany)

---
+++ BPVAPP-Backend/Controllers/CompanyController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BPVAPP_Backend.Database;
+using BPVAPP_Backend.Database.Models;
+
+namespace BPVAPP_Backend.Controllers
+{
+    public class CompanyController : Controller
+    {
+        public IActionResult CreateCompany(Company model)
+        {
+            var db = new DbConnection();
+            db.AddCompany(model);
+            return View();
+        }
+    }
+}
--- BPVAPP-Backend/Database/ModelMappings.cs
+++ BPVAPP-Backend/Database/ModelMappings.cs
@@ -1,9 +1,29 @@
-﻿using System;
+﻿using BPVAPP_Backend.Database.Models;
+using FluentNHibernate.Mapping;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace BPVAPP_Backend.Database
 {
-    // Place all the mappings of models in this.
+    public class CompanyMapping : ClassMap<Company>
+    {
+        public CompanyMapping()
+        {
+            Table("company_table");
+            Id(i => i.Id);
+            Map(i => i.Bedrijfsnaam).Length(255);
+            Map(i => i.Adres);
+            Map(i => i.Website).Default("0");
+            Map(i => i.Plaats);
+            Map(i => i.PostCode);
+            Map(i => i.TelefoonNummer);
+            Map(i => i.ContactPersoon_1);
+            Map(i => i.ContactPersoonEmail_1);
+            Map(i => i.ContactPersoon_2);
+            Map(i => i.ContactPersoonEmail_2);
+            Map(i => i.Opmerking);
+        }
+    }
 }
