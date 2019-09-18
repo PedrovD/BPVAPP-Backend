c7c595acc00d567a7b168f3024bd4101b837f34b
joostjasper <joostjasper@live.nl>
2019-09-18 15:51:35 +0200

    aangemaakt StageBegeleiderModel

---
+++ BPVAPP-Backend/Database/Models/StagebegeleiderModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BPVAPP_Backend.Database.Models
+{
+    public class StagebegeleiderModel
+    {
+        public virtual int Id { get; protected set; }
+        public virtual string FirstName { get; set; }
+        public virtual string LastName { get; set; }
+        public virtual int CatagoryId { get; set; }
+        public virtual string Mail { get; set; }
+        public virtual string Telephone { get; set; }
+        public virtual string Password { get; set; }
+
+    }
+}
