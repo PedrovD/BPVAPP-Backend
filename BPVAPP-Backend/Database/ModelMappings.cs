14cf83cfb5b9937f4204eb8790bb207aa6af56f5
joostjasper <joostjasper@live.nl>
2019-09-19 13:15:55 +0200

    company veranderd
    Contactpersoon is een eigen tabel nu die een bedrijfsid heeft

--- BPVAPP-Backend/Database/ModelMappings.cs
+++ BPVAPP-Backend/Database/ModelMappings.cs
@@ -8,24 +8,74 @@ using static BPVAPP_Backend.Database.Models.LeerlingModel;
 
 namespace BPVAPP_Backend.Database
 {
-
     public class CompanyMapping : ClassMap<Company>
     {
         public CompanyMapping()
         {
             Table("company_table");
             Id(i => i.Id);
-            Map(i => i.Bedrijfsnaam);
+            Map(i => i.Bedrijfsnaam).Length(255);
             Map(i => i.Adres);
-            Map(i => i.Website);
+            Map(i => i.Website).Default("0");
             Map(i => i.Plaats);
             Map(i => i.PostCode);
             Map(i => i.TelefoonNummer);
-            Map(i => i.ContactPersoon_1);
-            Map(i => i.ContactPersoonEmail_1);
-            Map(i => i.ContactPersoon_2);
-            Map(i => i.ContactPersoonEmail_2);
+            //Map(i => i.ContactPersoon_1);
+            //Map(i => i.ContactPersoonEmail_1);
+            //Map(i => i.ContactPersoon_2);
+            //Map(i => i.ContactPersoonEmail_2);
             Map(i => i.Opmerking);
         }
     }
+
+    public class CompanyLocationMapping : ClassMap<CompanyLocations>
+    {
+        public CompanyLocationMapping()
+        {
+            Table("company_location_table");
+            Id(i => i.Id);
+            Map(i => i.Locatie).Length(255);
+            Map(i => i.BedrijfId);
+        }
+    }
+
+
+    public class LeerlingModelMapping : ClassMap<LeerlingModel>
+    {
+        public LeerlingModelMapping()
+        {
+            Table("LeerlingModelTable");
+            Id(i => i.Id).GeneratedBy.Identity();
+            Map(i => i.FirstName);
+            Map(i => i.LastName);
+            Map(i => i.BegeleiderId);
+            Map(i => i.Postcode);
+            Map(i => i.Mail);
+            Map(i => i.Telephone);
+            Map(i => i.Password);
+            Map(i => i.CatagoryId);
+            HasOne(i => i.Begeleider);
+        }
+    }
+
+
+    public class ProductModelMapping : ClassMap<StagebegeleiderModel>
+    {
+        public ProductModelMapping()
+        {
+            Table("StagebegeleiderModelTable");
+            Id(i => i.Id).GeneratedBy.Identity();
+            Map(i => i.FirstName);
+            Map(i => i.LastName);
+            Map(i => i.LeerlingId);
+            Map(i => i.Mail);
+            Map(i => i.Telephone);
+            Map(i => i.Password);
+            Map(i => i.CatagoryId);
+            HasMany(i => i.leerlingen);
+        }
+    }
+
+
+
 }
---
+++ BPVAPP-Backend/Database/Models/CompanyLocations.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BPVAPP_Backend.Database.Models
+{
+    public class CompanyLocations
+    {
+        public virtual int Id { get; set; }
+        public virtual string Locatie { get; set; }
+        public virtual int BedrijfId { get; set; }
+    }
+}
