92611f6beffa6182582caa2d41394639d8114ead
joostjasper <joostjasper@live.nl>
2019-09-19 14:08:24 +0200

    telefoonnummer is een string

--- BPVAPP-Backend/Database/Models/CompanyContact.cs
+++ BPVAPP-Backend/Database/Models/CompanyContact.cs
@@ -10,10 +10,9 @@ namespace BPVAPP_Backend.Database.Models
     {
         public virtual int Id { get; set; }
         public virtual int BedrijfsId { get; set; }
-        public virtual int LocatieId { get; set; }
         public virtual string Naam { get; set; }
         public virtual string Email { get; set; }
-        public virtual int TelefoonNummer { get; set; }
+        public virtual string TelefoonNummer { get; set; }
         public virtual string Opmerking { get; set; }
     }
 }
