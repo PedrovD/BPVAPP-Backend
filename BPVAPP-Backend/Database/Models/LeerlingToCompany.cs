49043024af4a5c58679e72ac40a79392c43e192a
joostjasper <joostjasper@live.nl>
2019-09-19 14:25:02 +0200

    Koppeltabel aangemaakt

---
+++ BPVAPP-Backend/Database/Models/LeerlingToCompany.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BPVAPP_Backend.Database.Models
+{
+    public class LeerlingToCompany
+    {
+        public virtual int Id { get; set; }
+
+        public virtual string Postcode { get; set; }
+        public virtual string Plaats { get; set; }
+        public virtual string Staats { get; set; }
+        public virtual string Address { get; set; }
+        public virtual int CompanyId { get; set; }
+    }
+}
