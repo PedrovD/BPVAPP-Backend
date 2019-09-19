b42d9625138539496361a07149a81efdb4fe55b4
joostjasper <joostjasper@live.nl>
2019-09-19 15:39:16 +0200

    leerlingmodel

--- BPVAPP-Backend/Database/Models/LeerlingModel.cs
+++ BPVAPP-Backend/Database/Models/LeerlingModel.cs
@@ -5,21 +5,19 @@ using System.Threading.Tasks;
 
 namespace BPVAPP_Backend.Database.Models
 {
-        // This model is ussed for now as a refference for fluentnhibernate,
-        public class LeerlingModel
-        {
-            public virtual int Id { get; protected set; }
-            public virtual string FirstName { get; set; }
-            public virtual string LastName { get; set; }
-            public virtual int LocatieId { get; set; }            
-            public virtual int BegeleiderId { get; set; }
-            public virtual string StageperiodeStart { get; set; }
-            public virtual string StageperiodeEnd { get; set; }
-            public virtual string Postcode { get; set; }
-            public virtual string Mail { get; set; }
-            public virtual string Telephone { get; set; }
-            public virtual string Password { get; set; }
-            public object Begeleider { get; internal set; }
+    // This model is ussed for now as a refference for fluentnhibernate,
+    public class LeerlingModel
+    {
+        public virtual int Id { get; protected set; }
+        public virtual string voornaam { get; set; }
+        public virtual string tussenvoegsel { get; set; }
+        public virtual string achternaam { get; set; }
+        public virtual string woonplaats { get; set; }
+        public virtual string adres { get; set; }
+        public virtual string telefoonNr { get; set; }
+        public virtual string Postcode { get; set; }
+        public virtual string Mail { get; set; }
+        public virtual string Password { get; set; }
     }
         // Can be removed later when we are using real models
     
