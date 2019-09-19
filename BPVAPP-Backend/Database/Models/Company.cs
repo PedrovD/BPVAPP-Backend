c62d1d9c1ccdb5db9c3e9217eb54fc1f0b79df99
joostjasper <joostjasper@live.nl>
2019-09-19 13:18:43 +0200

    commit de contactpersoon dependancy
    contactpersoon gaat eigen tabel krijgen dus hoeft niet in bedrijftabel zelf anders veroorzaakt dit redundancy

--- BPVAPP-Backend/Database/Models/Company.cs
+++ BPVAPP-Backend/Database/Models/Company.cs
@@ -15,10 +15,10 @@ namespace BPVAPP_Backend.Database.Models
         public virtual string Plaats { get; set; }
         public virtual string PostCode { get; set; }
         public virtual int TelefoonNummer { get; set; }
-        public virtual string ContactPersoon_1 { get; set; }
-        public virtual string ContactPersoonEmail_1 { get; set; }
-        public virtual string ContactPersoon_2 { get; set; }
-        public virtual string ContactPersoonEmail_2 { get; set; }
+        //public virtual string ContactPersoon_1 { get; set; }
+        //public virtual string ContactPersoonEmail_1 { get; set; }
+        //public virtual string ContactPersoon_2 { get; set; }
+        //public virtual string ContactPersoonEmail_2 { get; set; }
         public virtual string Opmerking { get; set; }
     }
 }
