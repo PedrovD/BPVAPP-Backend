f3b6104d4be38db74e8e39ecadedffc25cef7d24
joostjasper <joostjasper@live.nl>
2019-09-19 14:08:04 +0200

    CompanyLocations
    meer details van de plek toegevoegd

--- BPVAPP-Backend/Database/Models/CompanyLocations.cs
+++ BPVAPP-Backend/Database/Models/CompanyLocations.cs
@@ -9,7 +9,11 @@ namespace BPVAPP_Backend.Database.Models
     public class CompanyLocations
     {
         public virtual int Id { get; set; }
-        public virtual string Locatie { get; set; }
-        public virtual int BedrijfId { get; set; }
+
+        public virtual string Postcode { get; set; }
+        public virtual string Plaats { get; set; }
+        public virtual string Staats { get; set; }
+        public virtual string Address { get; set; }
+        public virtual int CompanyId { get; set; }
     }
 }
