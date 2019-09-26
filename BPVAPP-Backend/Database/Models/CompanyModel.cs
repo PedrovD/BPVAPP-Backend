e0e62b529010dd4bd7a5973e35a9541fe3fa3cdd
Boris <b.mulder@appec.nl>
2019-09-26 12:18:03 +0200

    Create CompanyModel.cs
    renamed

---
+++ BPVAPP-Backend/Database/Models/CompanyModel.cs
@@ -0,0 +1,18 @@
+﻿namespace BPVAPP_Backend.Database.Models
+{
+    public class CompanyModel
+    {
+        public virtual int Id { get; set; }
+        public virtual string Bedrijfsnaam { get; set; }
+        public virtual string Adres { get; set; }
+        public virtual string Website { get; set; }
+        public virtual string Plaats { get; set; }
+        public virtual string PostCode { get; set; }
+        public virtual int TelefoonNummer { get; set; }
+        public virtual string ContactPersoon_1 { get; set; }
+        public virtual string ContactPersoonEmail_1 { get; set; }
+        public virtual string ContactPersoon_2 { get; set; }
+        public virtual string ContactPersoonEmail_2 { get; set; }
+        public virtual string Opmerking { get; set; }
+    }
+}
