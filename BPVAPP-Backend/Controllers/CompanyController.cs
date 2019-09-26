fdd861b5193148ac1598e24986012b9170734762
Boris <b.mulder@appec.nl>
2019-09-26 12:04:29 +0200

    Update CompanyController.cs
    able to select a company

--- BPVAPP-Backend/Controllers/CompanyController.cs
+++ BPVAPP-Backend/Controllers/CompanyController.cs
@@ -5,9 +5,14 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using BPVAPP_Backend.Database;
 using BPVAPP_Backend.Database.Models;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Bson;
+using Newtonsoft.Json.Linq;
 
 namespace BPVAPP_Backend.Controllers
 {
+    [Route("api/[controller]")]
     public class CompanyController : Controller
     {
         [HttpPost]
@@ -16,6 +21,31 @@ namespace BPVAPP_Backend.Controllers
             var db = new DbConnection();
             db.AddModel(model);
             return View();
+        }
+
+        [HttpGet]
+        [Route("companyId/{id}")]
+        public object GetCompanyById(int id)
+        {
+            try
+            {
+                JObject json = (JObject)JsonConvert.DeserializeObject(System.IO.File.ReadAllText("data.json"));
+
+                var rtn = json["Bedrijf"][id];
+
+                JObject company = new JObject();
+                JArray companyarr = new JArray
+                {
+                    json["Bedrijf"][id]
+                };
+                company["Bedrijf"] = companyarr;
+
+                return company.ToString();
+            }
+            catch (Exception e)
+            {
+                return e.ToString();
+            }
         }
     }
 }
