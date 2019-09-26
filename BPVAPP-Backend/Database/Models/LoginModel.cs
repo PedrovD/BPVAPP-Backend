b3392e2b9558138f35805d5fb6a2f547be53e3f5
Boris <b.mulder@appec.nl>
2019-09-26 12:05:54 +0200

    added
    login en register model

---
+++ BPVAPP-Backend/Database/Models/LoginModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BPVAPP_Backend.Database.Models
+{
+    public class LoginModel
+    {
+        public virtual string Email { get; set; }
+
+        public virtual string Password { get; set; }
+    }
+}
---
+++ BPVAPP-Backend/Database/Models/RegisterModel.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BPVAPP_Backend.Database.Models
+{
+    public class RegisterModel
+    {
+        public virtual string Email { get; set; }
+
+        public virtual string Password { get; set; }
+    }
+}
