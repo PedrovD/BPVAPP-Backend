9a00f19358c172af0b1e2a60dd9846c8d0ab08ca
PedrovD <pedro_vandouveren@hotmail.com>
2019-10-03 15:42:04 +0200

    Create StudentModel.cs

---
+++ BPVAPP-Backend/Database/Models/StudentModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BPVAPP_Backend.Database.Models
+{
+    public class StudentModel
+    {
+        public virtual int Id { get; set; }
+        public virtual int StudentNumber { get; set; }
+        public virtual string FrontName { get; set; }
+        public virtual string LastName { get; set; }
+        public virtual string Class { get; set; }
+    }
+}
