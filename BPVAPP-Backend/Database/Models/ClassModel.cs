9a15ccabfa179d5d527b7e653b01b2cca4236405
PedrovD <pedro_vandouveren@hotmail.com>
2019-10-03 15:41:52 +0200

    Added model: Class

---
+++ BPVAPP-Backend/Database/Models/ClassModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BPVAPP_Backend.Database.Models
+{
+    public class ClassModel
+    {
+        public virtual int Id { get; set; }
+        public virtual string Class { get; set; }
+    }
+}
