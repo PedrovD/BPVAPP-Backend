ba4017bdb0646393afcd8b7902a23ec36ee4b807
Boris <Borismulder@outlook.com>
2019-10-03 15:38:27 +0200

    Update RegisterModel.cs
    added username, frontend

--- BPVAPP-Backend/Database/Models/RegisterModel.cs
+++ BPVAPP-Backend/Database/Models/RegisterModel.cs
@@ -8,6 +8,8 @@ namespace BPVAPP_Backend.Database.Models
 {
     public class RegisterModel
     {
+        public virtual string UserName { get; set; }
+
         public virtual string Email { get; set; }
 
         public virtual string Password { get; set; }
