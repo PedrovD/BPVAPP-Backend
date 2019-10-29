5e8020e3f1c8d8e1c27fcbd993d5de3bd2d43e64
PedrovD <pedro_vandouveren@hotmail.com>
2019-10-29 14:40:40 +0100

    Update CompanyModel.cs

--- BPVAPP-Backend/Database/Models/CompanyModel.cs
+++ BPVAPP-Backend/Database/Models/CompanyModel.cs
@@ -9,6 +9,8 @@
         public virtual string Plaats { get; set; }
         public virtual string PostCode { get; set; }
         public virtual string TelefoonNummer { get; set; }
+        public virtual int Capacity { get; set; }
+        public virtual string CurrentCapacity { get; set; }
         public virtual string ContactPersoon_1 { get; set; }
         public virtual string ContactPersoonEmail_1 { get; set; }
         public virtual string ContactPersoon_2 { get; set; }
