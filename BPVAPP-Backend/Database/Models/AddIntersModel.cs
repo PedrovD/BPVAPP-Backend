2611420ac32ee1fe110364457615a0a939b25f0d
Boris <Borismulder@outlook.com>
2019-10-31 20:06:20 +0100

    Create AddIntersModel.cs
    a model for addding interns

---
+++ BPVAPP-Backend/Database/Models/AddIntersModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BPVAPP_Backend.Database.Models
+{
+    public class AddIntersModel
+    {
+        public string[] Students { get; set; }
+        public string[] StartDates { get; set; }
+        public string[] EndDates { get; set; }
+    }
+}
