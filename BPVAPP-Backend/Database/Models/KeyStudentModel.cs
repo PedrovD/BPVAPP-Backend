54723a4bbb2a2d663389c6786ffc131fa3787d0d
Boris <Borismulder@outlook.com>
2019-11-01 10:44:13 +0100

    Create KeyStudentModel.cs
    custom return type model

---
+++ BPVAPP-Backend/Database/Models/KeyStudentModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BPVAPP_Backend.Database.Models
+{
+    public class KeyStudentModel
+    {
+        public  int Id { get; set; }
+        public  string StudentNumber { get; set; }
+        public  string FirstName { get; set; }
+        public  string TussenVoegsel { get; set; }
+        public  string LastName { get; set; }
+        public  string Woonplaats { get; set; }
+        public  string Adres { get; set; }
+        public  string Class { get; set; }
+        public  string PhoneNumber { get; set; }
+        public string HasInternship { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int Key { get; set; }
+    }
+}
